05d605bae7d0d8f117c3989739620e7934a9e559
Edu Calvo <educlopez93@gmail.com>
2024-10-13 13:11:01 +0200

    together blink example but with submit button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,46 @@
+import Providers from "@/app/providers";
+import bgPattern from "@/public/bg-pattern-transparent.png";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
+
+let title = "BlinkShot – Real-Time AI Image Generator";
+let description = "Generate images with AI in a milliseconds";
+let url = "https://www.blinkshot.io/";
+let ogimage = "https://www.blinkshot.io/og-image.png";
+let sitename = "blinkshot.io";
 
 export const metadata: Metadata = {
-  title: "Horror Story Generator",
-  description: "Generate visual horror stories with AI",
+  metadataBase: new URL(url),
+  title,
+  description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    images: [ogimage],
+    title,
+    description,
+    url: url,
+    siteName: sitename,
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [ogimage],
+    title,
+    description,
+  },
 };
 
 export default function RootLayout({
@@ -15,9 +49,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-black text-white min-h-screen`}>
-        {children}
+    <html lang="en" className="h-full">
+      <head>
+        <meta name="color-scheme" content="dark" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} dark h-full min-h-full bg-[length:6px] font-mono text-gray-100 antialiased`}
+        style={{ backgroundImage: `url(${bgPattern.src}` }}
+      >
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,244 @@
-// import StoryEditor from "@/components/StoryEditor";
-import { HorrorStoryCreator } from "@/components/HorrorStoryCreator";
+"use client";
+
+import GithubIcon from "@/app/components/icons/github-icon";
+import XIcon from "@/app/components/icons/x-icon";
+import Logo from "@/app/components/logo";
+import Spinner from "@/app/components/spinner";
+import { Button } from "@/app/components/ui/button";
+import { Input } from "@/app/components/ui/input";
+import { Switch } from "@/app/components/ui/switch";
+import { Textarea } from "@/app/components/ui/textarea";
+import imagePlaceholder from "@/public/image-placeholder.png";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import { useState } from "react";
+
+type ImageResponse = {
+  b64_json: string;
+  timings: { inference: number };
+};
 
 export default function Home() {
+  const [prompt, setPrompt] = useState("");
+  const [submittedPrompt, setSubmittedPrompt] = useState("");
+  const [iterativeMode, setIterativeMode] = useState(false);
+  const [userAPIKey, setUserAPIKey] = useState("");
+  const [generations, setGenerations] = useState<
+    { prompt: string; image: ImageResponse }[]
+  >([]);
+  let [activeIndex, setActiveIndex] = useState<number>();
+
+  const {
+    data: image,
+    isFetching,
+    refetch,
+  } = useQuery({
+    queryKey: [submittedPrompt],
+    queryFn: async () => {
+      let res = await fetch("/api/generateImages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt: submittedPrompt,
+          userAPIKey,
+          iterativeMode,
+        }),
+      });
+
+      if (!res.ok) {
+        throw new Error(await res.text());
+      }
+      return (await res.json()) as ImageResponse;
+    },
+    enabled: false,
+    staleTime: Infinity,
+    retry: false,
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmittedPrompt(prompt);
+    setTimeout(() => refetch(), 0);
+  };
+
+  if (image && !generations.map((g) => g.image).includes(image)) {
+    setGenerations((images) => [...images, { prompt: submittedPrompt, image }]);
+    setActiveIndex(generations.length);
+  }
+
+  let activeImage =
+    activeIndex !== undefined ? generations[activeIndex].image : undefined;
+
   return (
-    // <main className="container mx-auto px-4 py-8">
-    //   <h1 className="text-4xl font-bold text-center mb-8 text-red-600">
-    //     Visual Horror Story Generator
-    //   </h1>
-    //   <StoryEditor />
-    // </main>
-    <div className="min-h-screen bg-gray-900">
-      <HorrorStoryCreator />
+    <div className="flex h-full flex-col px-5">
+      <header className="flex justify-center pt-20 md:justify-end md:pt-3">
+        <div className="absolute left-1/2 top-6 -translate-x-1/2">
+          <a href="https://www.dub.sh/together-ai" target="_blank">
+            <Logo />
+          </a>
+        </div>
+        <div>
+          <label className="text-xs text-gray-200">
+            [Optional] Add your{" "}
+            <a
+              href="https://api.together.xyz/settings/api-keys"
+              target="_blank"
+              className="underline underline-offset-4 transition hover:text-blue-500"
+            >
+              Together API Key
+            </a>{" "}
+          </label>
+          <Input
+            placeholder="API Key"
+            type="password"
+            value={userAPIKey}
+            className="mt-1 bg-gray-400 text-gray-200 placeholder:text-gray-300"
+            onChange={(e) => setUserAPIKey(e.target.value)}
+          />
+        </div>
+      </header>
+
+      <div className="flex justify-center">
+        <form className="mt-10 w-full max-w-lg" onSubmit={handleSubmit}>
+          <fieldset>
+            <div className="relative">
+              <Textarea
+                rows={4}
+                spellCheck={false}
+                placeholder="Describe your image..."
+                required
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                className="w-full resize-none border-gray-300 border-opacity-50 bg-gray-400 px-4 text-base placeholder-gray-300"
+              />
+            </div>
+
+            <div className="mt-3 flex justify-between items-center">
+              <Button type="submit" disabled={isFetching}>
+                {isFetching ? <Spinner className="size-4 mr-2" /> : null}
+                Generate Image
+              </Button>
+              <label
+                title="Use earlier images as references"
+                className="inline-flex items-center gap-2"
+              >
+                Consistency mode
+                <Switch
+                  checked={iterativeMode}
+                  onCheckedChange={setIterativeMode}
+                />
+              </label>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+
+      <div className="flex w-full grow flex-col items-center justify-center pb-8 pt-4 text-center">
+        {!activeImage || !prompt ? (
+          <div className="max-w-xl md:max-w-4xl lg:max-w-3xl">
+            <p className="text-xl font-semibold text-gray-200 md:text-3xl lg:text-4xl">
+              Generate images in real-time
+            </p>
+            <p className="mt-4 text-balance text-sm text-gray-300 md:text-base lg:text-lg">
+              Enter a prompt and generate images in milliseconds as you type.
+              Powered by Flux on Together AI.
+            </p>
+          </div>
+        ) : (
+          <div className="mt-4 flex w-full max-w-4xl flex-col justify-center">
+            <div>
+              <Image
+                placeholder="blur"
+                blurDataURL={imagePlaceholder.blurDataURL}
+                width={1024}
+                height={768}
+                src={`data:image/png;base64,${activeImage.b64_json}`}
+                alt=""
+                className={`${isFetching ? "animate-pulse" : ""} max-w-full rounded-lg object-cover shadow-sm shadow-black`}
+              />
+            </div>
+
+            <div className="mt-4 flex gap-4 overflow-x-scroll pb-4">
+              {generations.map((generatedImage, i) => (
+                <button
+                  key={i}
+                  className="w-32 shrink-0 opacity-50 hover:opacity-100"
+                  onClick={() => setActiveIndex(i)}
+                >
+                  <Image
+                    placeholder="blur"
+                    blurDataURL={imagePlaceholder.blurDataURL}
+                    width={1024}
+                    height={768}
+                    src={`data:image/png;base64,${generatedImage.image.b64_json}`}
+                    alt=""
+                    className="max-w-full rounded-lg object-cover shadow-sm shadow-black"
+                  />
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      <footer className="mt-16 w-full items-center pb-10 text-center text-gray-300 md:mt-4 md:flex md:justify-between md:pb-5 md:text-xs lg:text-sm">
+        <p>
+          Powered by{" "}
+          <a
+            href="https://www.dub.sh/together-ai"
+            target="_blank"
+            className="underline underline-offset-4 transition hover:text-blue-500"
+          >
+            Together.ai
+          </a>{" "}
+          &{" "}
+          <a
+            href="https://dub.sh/together-flux"
+            target="_blank"
+            className="underline underline-offset-4 transition hover:text-blue-500"
+          >
+            Flux
+          </a>
+        </p>
+
+        <div className="mt-8 flex items-center justify-center md:mt-0 md:justify-between md:gap-6">
+          <p className="hidden whitespace-nowrap md:block">
+            100% free and{" "}
+            <a
+              href="https://github.com/Nutlope/blinkshot"
+              target="_blank"
+              className="underline underline-offset-4 transition hover:text-blue-500"
+            >
+              open source
+            </a>
+          </p>
+
+          <div className="flex gap-6 md:gap-2">
+            <a href="https://github.com/Nutlope/blinkshot" target="_blank">
+              <Button
+                variant="outline"
+                size="sm"
+                className="inline-flex items-center gap-2"
+              >
+                <GithubIcon className="size-4" />
+                GitHub
+              </Button>
+            </a>
+            <a href="https://x.com/nutlope" target="_blank">
+              <Button
+                size="sm"
+                variant="outline"
+                className="inline-flex items-center gap-2"
+              >
+                <XIcon className="size-3" />
+                Twitter
+              </Button>
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
